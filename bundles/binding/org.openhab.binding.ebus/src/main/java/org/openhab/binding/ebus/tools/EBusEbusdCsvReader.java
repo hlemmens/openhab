c4a8b5ae6b58cffc4207636a42f24c82191eb65b
Christian S. <info@cs-dev.de>
2016-11-29 21:03:45 +0100

    update

--- bundles/binding/org.openhab.binding.ebus/src/main/java/org/openhab/binding/ebus/tools/EBusEbusdCsvReader.java
+++ bundles/binding/org.openhab.binding.ebus/src/main/java/org/openhab/binding/ebus/tools/EBusEbusdCsvReader.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Pattern;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.openhab.binding.ebus.internal.configuration.TelegramConfiguration;
@@ -97,36 +99,92 @@ public class EBusEbusdCsvReader {
                     t.setPos(9);
                 }
 
-                if (datatype.equals("temp")) {
-                    t.setType("data2c");
-                    values.put(name, t);
+                final String d1cTypes[] = { "D1C", "percent1", "temp1", "hours1", "calibration1" };
+                final String d2bTypes[] = { "D2B", "temp2", "press", "percent2" };
+                final String d2cTypes[] = { "D2C", "temp", "percent", "calibration", "tempsensor" };
 
-                } else if (datatype.equals("temp0")) {
-                    t.setType("uchar");
-                    values.put(name, t);
+                final String ucharTypes[] = { "UCH", "onoff", "temp0", "unit", "fueltype", "countertype",
+                        "opdatacyclic", "opdatatransfer", "opdataheat", "opdataaction", "desiredtemp", "percent0",
+                        "daysel", "days", "minutes0", "hours", "hour", "seconds0", "calibration0", "mcmode", "mctype",
+                        "hwcmode", "status", "yesno", "errorok", "sensor", "backtype", "backmode", "power", "pumppow",
+                        "pumpstate", "dcfstate", "phaseok", "switchvalve", "hpstatus" };
 
-                } else if (datatype.equals("temp1")) {
+                final String charTypes[] = { "SCH", "outsidetemp", "percents", "temps", "calibrations" };
+
+                final String intTypes[] = { "SIN", "temps2", "integral" };
+                final String uintTypes[] = { "UIN", "minutes2", "hoursum2", "seconds2", "uin100", "curve", "energy",
+                        "cntstarts2", "frequency" };
+
+                if (ArrayUtils.contains(d1cTypes, datatype)) {
                     t.setType("data1c");
                     values.put(name, t);
 
-                } else if (datatype.equals("temp2")) {
+                } else if (ArrayUtils.contains(d2bTypes, datatype)) {
                     t.setType("data2b");
                     values.put(name, t);
 
-                } else if (datatype.equals("tempsensor")) {
+                } else if (ArrayUtils.contains(d2cTypes, datatype)) {
                     t.setType("data2c");
                     values.put(name, t);
 
+                } else if (ArrayUtils.contains(ucharTypes, datatype)) {
+                    t.setType("uchar");
+                    values.put(name, t);
+
+                } else if (ArrayUtils.contains(charTypes, datatype)) {
+                    t.setType("char");
+                    values.put(name, t);
+
+                } else if (ArrayUtils.contains(uintTypes, datatype)) {
+                    t.setType("uint");
+                    values.put(name, t);
+
+                } else if (ArrayUtils.contains(intTypes, datatype)) {
+                    t.setType("int");
+                    values.put(name, t);
+
+                } else if (datatype.equals("cntminutes")) {
+                    datatype = "BCD";
+
+                } else if (datatype.equals("cnthours") || datatype.equals("cnthours")) {
+                    datatype = "BCD:3";
+
+                } else if (datatype.equals("cntfuel")) {
+                    datatype = "BCD:4";
+
+                }
+
+                if (datatype.startsWith("BCD")) {
+                    String substring = datatype.substring(datatype.indexOf(':'));
+                    System.out.println("EBusEbusdCsvReader.run()");
+                }
+
+                /*
+                 * cntminutes BCD
+                 * cnthours BCD:3
+                 * cntstarts BCD:3
+                 * cntfuel BCD:4
+                 *
+                 */
+
+                if (datatype.equals("calibrations") || datatype.equals("seconds")) {
+                    t.setFactor(new BigDecimal("0.5"));
+                }
+
+                if (datatype.equals("curve") || datatype.equals("uin100")) {
+                    t.setFactor(new BigDecimal("0.01"));
+                }
+
+                if (datatype.equals("minutes")) {
+                    t.setFactor(new BigDecimal("0.0083333333333333"));
+                }
+
+                if (datatype.equals("tempsensor")) {
                     TelegramValue t2 = new TelegramValue();
                     t2.setPos(t.getPos() + 2);
                     t2.setType("uchar");
                     t2.setLabel("Status " + t.getLabel());
                     values.put("status", t2);
-
-                } else if (datatype.equals("onoff")) {
-                    t.setType("uchar");
-                    values.put(name, t);
-
                 }
 
                 if (type.startsWith("*")) {
