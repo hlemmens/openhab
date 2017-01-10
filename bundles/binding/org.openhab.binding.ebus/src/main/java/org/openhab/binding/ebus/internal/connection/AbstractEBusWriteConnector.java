70be4882403d764c9b06a599aca2560d5f5d5d34
Christian S. <info@cs-dev.de>
2017-01-10 18:57:08 +0100

    Just formating

--- bundles/binding/org.openhab.binding.ebus/src/main/java/org/openhab/binding/ebus/internal/connection/AbstractEBusWriteConnector.java
+++ bundles/binding/org.openhab.binding.ebus/src/main/java/org/openhab/binding/ebus/internal/connection/AbstractEBusWriteConnector.java
@@ -61,7 +61,7 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /**
      * Returns the eBus binding used sender Id
-     * 
+     *
      * @return
      */
     public byte getSenderId() {
@@ -70,7 +70,7 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /**
      * Set the eBus binding sender Id
-     * 
+     *
      * @param senderId The new id, default is 0xFF
      */
     public void setSenderId(byte senderId) {
@@ -79,7 +79,7 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.openhab.binding.ebus.internal.connection.AbstractEBusConnector#connect()
      */
     @Override
@@ -99,7 +99,7 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /**
      * Add a byte array to send queue.
-     * 
+     *
      * @param data
      * @return
      */
@@ -174,7 +174,7 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /**
      * Internal send function. Send and read to detect byte collisions.
-     * 
+     *
      * @param secondTry
      * @throws IOException
      */
@@ -380,7 +380,7 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /**
      * Resend data if it's the first try or call resetSend()
-     * 
+     *
      * @param secondTry
      * @return
      * @throws IOException
@@ -416,7 +416,7 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /**
      * Called event if a SYN packet has been received
-     * 
+     *
      * @throws IOException
      */
     @Override
@@ -435,7 +435,7 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /**
      * Writes one byte to the backend
-     * 
+     *
      * @param b
      * @throws IOException
      */
@@ -443,14 +443,14 @@ public abstract class AbstractEBusWriteConnector extends AbstractEBusConnector {
 
     /**
      * Resets the input buffer of input stream
-     * 
+     *
      * @throws IOException
      */
     protected abstract void resetInputBuffer() throws IOException;
 
     /**
      * Return the undelaying input stream
-     * 
+     *
      * @return
      */
     protected abstract InputStream getInputStream();
